ad2e4c132fce491f544747f94edbf03aa5acacb4
haohao_1522 <56289825+steinsgate97122@users.noreply.github.com>
2023-07-14 16:46:11 +0800

    Add files via upload

--- JS/1.helloworld/js/3.datatype.js
+++ JS/1.helloworld/js/3.datatype.js
@@ -63,14 +63,47 @@ console.log(isNaN(NaN));
 * java中使用BigDecimal进行比较
 * js中可以使用Math.abs计算两数之差的误差
 * */
-console.log((1 / 3) == (1 - 2 / 3));
+console.log((1 / 3) === (1 - 2 / 3));
 console.log(Math.abs(1 / 3 - (1 - 2 / 3)) < 1e-10);
 
 // 数组元素类型不需要相同，访问数组元素直接加下标
-var arr = [1, "abc", null, true];
+var arr = [1, "abc", null, true, 1];
 console.log(arr);
 console.log(arr[0]);
 console.log(arr[3]);
+// arr.length获取数组长度，可改变，超出部分为undefined
+console.log(arr.length);
+arr.length = 6;
+console.log(arr.length);
+console.log(arr);
+console.log(arr[5]);
+// arr.indexOf() 通过元素内容获取下标
+console.log(arr.indexOf(1));
+// arr.slice() 切片，取头不取尾
+console.log(arr.slice(1, 3));
+// push() pop() 数组尾部  unshift() shift() 数组头部
+arr.push("p");
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift("s");
+console.log(arr);
+arr.shift();
+console.log(arr);
+// sort() 排序 reserve() 反转
+arr = ['C', 'A', 'B'];
+console.log(arr.sort());
+console.log(arr.reverse());
+// concat() 拼接数组，未改变原数组
+console.log(arr.concat([1, 2]));
+// join() 数组拼接为字符串
+console.log(arr.join("-"));
+// 支持多维数组
+var arr2 = [[1,2,3],[4,5,6],["7",true,false]];
+console.log(arr2[2][1]);
+arr2[2][1] = null;
+console.log(arr2);
+
 
 // 对象直接用大括号定义，访问对象属性用.
 var person = {
@@ -78,6 +111,20 @@ var person = {
     age: 26,
     tags: ["baidu", 'dhu', true]
 };
-
+person.name = "zhangzhehao01";
 console.log(person);
-console.log(person.name);
+// 可访问不存在的属性，返回undefined
+console.log(person.address);
+// 可delete掉某个属性
+delete person.age;
+console.log(person);
+// 可直接加属性
+person.address = "shanghai";
+console.log(person);
+// js中，方法本质上是对象的属性，只是属性的值为函数类型
+// in 判断属性是否在对象中存在；toString() hasOwnProperty()都是继承的方法
+// hasOwnProperty()可判断一个属性是否为对象自己的
+console.log("name" in person);
+console.log("toString" in person);
+console.log(person.hasOwnProperty("name"));
+console.log(person.hasOwnProperty("toString"));
